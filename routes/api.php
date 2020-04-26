39568262a45fe5ed60ca7bb0b114bb587862bf68
Joker_V <joker_v_@ukr.net>
2020-04-26 16:18:05 +0300

    Creating the Initial Components without data

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Buyers
+Route::apiResource('buyers', 'Buyer\BuyerController')->only(['index', 'show']);
+
+//Categories
+Route::apiResource('categories', 'Category\CategoryController');
+
+//Products
+Route::apiResource('products', 'Product\ProductController')->only(['index', 'show']);
+
+//Sellers
+Route::apiResource('sellers', 'Seller\SellerController')->only(['index', 'show']);
+
+//Transactions
+Route::apiResource('transactions', 'Transaction\TransactionController')->only(['index', 'show']);
+
+//Users
+Route::apiResource('users', 'User\UserController');
